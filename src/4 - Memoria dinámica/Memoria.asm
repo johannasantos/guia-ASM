9c6484b721addafb6822707fc01a2735b48e5c02
johannasantos <johannasabsantos@gmail.com>
2025-05-04 23:34:52 +0000

    add test memoria str len

--- src/4 - Memoria dinámica/Memoria.asm
+++ src/4 - Memoria dinámica/Memoria.asm
@@ -31,7 +31,36 @@ strPrint:
 	ret
 
 ; uint32_t strLen(char* a)
+
+; RDI -> char* a
+; EAX -> cantidad
+; R10B -> letra_actual
+;
 strLen:
+	push RBP
+	mov RBP, RSP
+
+	mov EAX, 0 ; me aseguro de que si tiene que terminar la ejecucion en el final, devuelve 0
+
+	cmp RDI, 0 ; if(a == null)
+	je final_str_len
+
+	mov R10B, [RDI]; letra_actual = (*a);
+
+bucle_letra_actual_str_len:
+	cmp R10B, 0 
+	je final_str_len
+
+	inc EAX ; cantidad++;
+	inc RDI ; a++;
+
+	mov R10B, [RDI] ; letra_actual = (*a);
+
+	jmp bucle_letra_actual_str_len
+
+final_str_len:
+
+	pop RBP
 	ret
 
 
