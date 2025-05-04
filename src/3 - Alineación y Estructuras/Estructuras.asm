a36700e1133513250b30fc989e80fc367b757003
johannasantos <johannasabsantos@gmail.com>
2025-05-04 22:11:02 +0000

    agrego segundo ejercicio estructuras

--- src/3 - Alineación y Estructuras/Estructuras.asm
+++ src/3 - Alineación y Estructuras/Estructuras.asm
@@ -2,17 +2,17 @@
 
 ;########### ESTOS SON LOS OFFSETS Y TAMAÑO DE LOS STRUCTS
 ; Completar las definiciones (serán revisadas por ABI enforcer):
-NODO_OFFSET_NEXT EQU 8
+NODO_OFFSET_NEXT EQU 0
 NODO_OFFSET_CATEGORIA EQU 8
-NODO_OFFSET_ARREGLO EQU 8
-NODO_OFFSET_LONGITUD EQU 8
-NODO_SIZE EQU 8
+NODO_OFFSET_ARREGLO EQU 16
+NODO_OFFSET_LONGITUD EQU 24
+NODO_SIZE EQU 32
 PACKED_NODO_OFFSET_NEXT EQU 8
 PACKED_NODO_OFFSET_CATEGORIA EQU 8
 PACKED_NODO_OFFSET_ARREGLO EQU 8
 PACKED_NODO_OFFSET_LONGITUD EQU 8
 PACKED_NODO_SIZE EQU 8
-LISTA_OFFSET_HEAD EQU 8
+LISTA_OFFSET_HEAD EQU 0
 LISTA_SIZE EQU 8
 PACKED_LISTA_OFFSET_HEAD EQU 8
 PACKED_LISTA_SIZE EQU 8
@@ -30,7 +30,36 @@ global cantidad_total_de_elementos_packed
 ;########### DEFINICION DE FUNCIONES
 ;extern uint32_t cantidad_total_de_elementos(lista_t* lista);
 ;registros: lista[?]
+
+; RDI -> lista_t* lista
+; R10 -> nodo_puntero_actual
+; EAX -> contador
+;
+
 cantidad_total_de_elementos:
+	; prologo
+	push RBP
+	mov RBP, RSP
+
+	mov EAX, 0 ; inicializarlo en el eax el contador
+	
+	cmp RDI, 0 ; if(lista == null) { return 0; }
+	je final
+
+	mov R10, [RDI + LISTA_OFFSET_HEAD] ; node_s* nodo_puntero_actual = (*lista).head;
+
+recorrer_lista:
+	cmp R10, 0 ; nodo_puntero_actual =! null
+	je final 
+
+	add EAX, [R10 + NODO_OFFSET_LONGITUD] ; contador += (*nodo_puntero_actual).longitud;
+	mov R10, [R10 + NODO_OFFSET_NEXT] ; nodo_puntero_actual = (*nodo_puntero_actual).next;
+	
+	jmp recorrer_lista ; salta a recorrer_lista para seguir iterando 
+
+final:
+	; epilogo
+	pop RBP
 	ret
 
 ;extern uint32_t cantidad_total_de_elementos_packed(packed_lista_t* lista);
